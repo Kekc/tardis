4f6b46c7e677d8f756a8a43cfaa72aa17723084f
Kekc <makc.kekc@gmail.com>
2015-01-17 17:42:10 +0500

    very alpha ajax load

---
+++ blog/static/js/blog.js
@@ -0,0 +1,48 @@
+
+
+var load_posts = function(data){
+	"use strict";
+
+	console.log(data);
+	var author = data['author'],
+		category = data['category'],
+		query = data['query'],
+		load_url = data['load_url'],
+		load_flag = data['load_flag'],
+		$button = $('#post-load-button'),
+		offset = $button.data('offset'),
+		new_offset = offset + 3;
+
+	$button.data('offset', new_offset);
+
+	$.ajax({
+			url: '/',
+		data: {
+			offset: new_offset,
+			category: category,
+			author: author,
+			query: query
+		}
+
+		})
+		.done(function(data) {
+			console.log(data);
+			var load_flag = data['load_flag'],
+				posts = data['posts'],
+				$post_container = $('.posts'),
+				template = Handlebars.compile( $('#post_tpl').html());
+			if (!load_flag) {
+				$button.hide();
+			}
+			if (posts) {
+				for (var i=0; i<posts.length; i++) {
+					console.log(posts[i]);
+					$post_container.append(template(posts[i]));
+				}
+			}
+		})
+		.fail(function(e) {
+			console.log('fail');
+		})
+
+};
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.core.urlresolvers import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
@@ -12,10 +14,94 @@ from blog.models import Category, Post, User
 from blog.forms import RegistrationForm, LoginForm
 
 
-class IndexView(ListView):
+class JSONResponseMixin(object):
+
+    def render_to_json_response(self, context, user, **response_kwargs):
+        """
+        Returns a JSON response, transforming 'context' to make the payload.
+        """
+        return JsonResponse(
+            self.get_data(context, user), safe=False,
+            **response_kwargs
+        )
+
+    def get_data(self, context, user):
+        """
+        Returns an object that will be serialized as JSON by json.dumps().
+        """
+
+        print 'context', context
+        items = context['posts']
+        posts = [{
+            'id': item.id,
+            'title': item.title,
+            'text': item.text,
+            'author_name': item.author.username,
+            'author_url': reverse('author', args=(item.author.id,)),
+            'edit_url': reverse('edit_post', args=(item.id,)),
+            'edit_flag': user == item.author or user.is_superuser,
+            'categories': [{'url': reverse('category', args=(category.id,)), 'name': category.name} for category in item.categories.all()],
+
+        } for item in items]
+        data = {
+            'posts': posts,
+            'load_flag': context['load_flag'],
+        }
+        # result = [{'id': item.id} for item in objects]
+        # return list(objects.values('id', 'title', 'author', 'text', 'created', 'categories'))
+        return data
+
+class IndexView(JSONResponseMixin, ListView):
     template_name = 'main.html'
     context_object_name = 'posts'
     queryset = Post.objects.all().order_by('-created')[:3]
+
+    def get_queryset(self):
+        print 'queryset'
+        print self.request.GET
+        self.offset = int(self.request.GET.get('offset', 0))
+        print self.offset
+        qs = Post.objects.all().order_by('-created')
+        self.load_flag = True if qs[self.offset+3:self.offset+4] else False
+        return qs[self.offset:self.offset+3]
+
+    def get_context_data(self, **kwargs):
+        print 'append'
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['load_flag'] = self.load_flag
+        context['ajax_data'] = json.dumps({
+            # 'offset': self.offset,
+            'category': None,
+            'author': None,
+            'query': None,
+            'load_url': reverse('ajax_load'),
+        })
+        # context['offset'] = self.offset
+        # context['category'] = None
+        # context['author'] = None
+        # context['query'] = None
+        # context['load_url'] = reverse('ajax_load')
+
+        return context
+
+    def render_to_response(self, context):
+        print 'response'
+        if self.request.is_ajax():
+            return self.render_to_json_response(context, self.request.user)
+        else:
+            return super(IndexView, self).render_to_response(context)
+
+    # def get(self, request, *args, **kwargs):
+    #     # view = super(IndexView, self).get(request, *args, **kwargs)
+    #     if request.is_ajax():
+    #         print self.request.GET
+    #         offset = int(request.GET.get('offset'))
+    #         category = request.GET.get('category')
+    #         author = request.GET.get('author')
+    #         query = request.GET.get('category')
+    #         return self.get_json_response(self.convert_context_to_json(context))
+    #     else:
+    #         return super(IndexView, self).get(request, *args, **kwargs)
 
 
 class CategoryView(ListView):
@@ -53,13 +139,11 @@ class SearchView(ListView):
     def get_queryset(self):
         self.query = self.request.GET.get('search')
         query = self.query.strip().split(' ')
-        print query
         if query:
             query_list = [(Q(text__contains=word) | Q(title__contains=word) |
                 Q(categories__name__contains=word)) for word in query]
             query = reduce(lambda a, b: a & b, query_list)
             qs = Post.objects.filter(query).distinct()
-            print query
         else:
             qs = Post.objects.all()
         return qs.order_by('-created')[:3]
@@ -68,6 +152,20 @@ class SearchView(ListView):
         context = super(SearchView, self).get_context_data(**kwargs)
         context['query'] = self.query
         return context
+
+
+
+
+
+class AjaxListView(JSONResponseMixin, ListView):
+
+    def get(self, request):
+        if request.is_ajax():
+            offset = int(request.GET.get('offset'))
+            category = request.GET.get('category')
+            author = request.GET.get('author')
+            query = request.GET.get('category')
+            return self.render_to_response({'data': 1})
 
 
 class PostCreateView(CreateView):
